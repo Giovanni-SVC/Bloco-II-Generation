9c79ea71cf4cec4a086a23644994c4a385fed8f1
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2021-06-25 09:50:31 -0300

    Atualizando Código BlogPessoal Spring Boot

--- Spring Boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ Spring Boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -1,7 +1,11 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +20,8 @@ public class PostagemController {
 	private PostagemRepository repository; 
 	
 	@GetMapping
-	public String mensagem() {
-		return "Olá! Estou aprendendo Spring Boot!";
+	public ResponseEntity<List<Postagem>> GetAll (){ 
+		return ResponseEntity.ok(repository.findAll());
 	}
 	
 }
