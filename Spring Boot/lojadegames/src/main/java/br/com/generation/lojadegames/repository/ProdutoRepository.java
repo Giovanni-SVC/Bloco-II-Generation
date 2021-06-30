e82fd71ca6b0dc5386e0ab796835a161df20c7d7
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2021-06-30 12:25:14 -0300

    Adicionando Projeto Loja de Games

---
+++ Spring Boot/lojadegames/src/main/java/br/com/generation/lojadegames/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package br.com.generation.lojadegames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.generation.lojadegames.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	public List <Produto> findAllByNomeIgnoreCase(String nome);
+
+}
