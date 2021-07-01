b21273dfcb30619e9aba3c3acd6f5e3855a9f064
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2021-07-01 16:50:18 -0300

    Atualizando anotação CategoriaRepository

--- Spring Boot/lojadegames/src/main/java/br/com/generation/lojadegames/repository/CategoriaRepository.java
+++ Spring Boot/lojadegames/src/main/java/br/com/generation/lojadegames/repository/CategoriaRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.generation.lojadegames.model.Categoria;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 	
 	public List <Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
