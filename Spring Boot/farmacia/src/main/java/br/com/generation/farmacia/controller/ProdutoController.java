65a056f9c2a293547837578f4cf50627fab6ee6b
Giovanni <80859834+Giovanni-SVC@users.noreply.github.com>
2021-07-01 15:55:13 -0300

    Adicionando Projeto Farmárcia

---
+++ Spring Boot/farmacia/src/main/java/br/com/generation/farmacia/controller/ProdutoController.java
@@ -0,0 +1,64 @@
+package br.com.generation.farmacia.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.generation.farmacia.model.Produto;
+import br.com.generation.farmacia.repository.ProdutoRepository;
+
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoRepository repository;
+	
+	@GetMapping
+	public ResponseEntity <List<Produto>> findAllProduto(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity <Produto> findByIdProduto(@PathVariable long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity <List<Produto>> findByTituloProduto (@PathVariable String titulo){
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity <Produto> post (@RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity <Produto> put (@RequestBody Produto produto){
+		return ResponseEntity.ok(repository.save(produto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
+	
+
+}
---
+++ Spring Boot/farmacia/src/main/java/br/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,80 @@
+package br.com.generation.farmacia.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
+	
+	@NotNull
+	private String departamento;
+	
+	@NotNull
+	private String ramo;
+	
+	@NotNull
+	private String descricao;
+	
+
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public String getRamo() {
+		return ramo;
+	}
+
+	public void setRamo(String ramo) {
+		this.ramo = ramo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
+
+}
